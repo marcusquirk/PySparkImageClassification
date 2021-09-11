38e9dc73979d8c9a3b0e5dea68c0653c8c345522
Marcus <48571488+marcusquirk@users.noreply.github.com>
2021-09-11 14:37:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,57 @@
-# This is a sample Python script.
+# necessary import 
+from pyspark.sql import SparkSession
+from pyspark.ml.image import ImageSchema
+from pyspark.sql.functions import lit
+from functools import reduce
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml import Pipeline
+import sparkdl
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# create a spark session
+spark = SparkSession.builder.appName('DigitRecog').getOrCreate()
+
+# loaded image
+df = spark.read.format("image").load("./NumtaDB/training-a/", inferSchema = True)
+print('hello')
+df.printSchema()
+
+'''one = read.format("1").withColumn("label", lit(1))
+two = read.format("2").withColumn("label", lit(2))
+three = read.format("3").withColumn("label", lit(3))
+four = read.format("4").withColumn("label", lit(4))
+five = read.format("5").withColumn("label", lit(5))
+six = read.format("6").withColumn("label", lit(6))
+seven = read.format("7").withColumn("label", lit(7))
+eight =read.format("8").withColumn("label", lit(8))
+nine = read.format("9").withColumn("label", lit(9))
+
+dataframes = [zero, one, two, three,four,
+             five, six, seven, eight, nine]'''
+
+'''# merge data frame
+df = reduce(lambda first, second: first.union(second), dataframes)'''
+
+# repartition dataframe 
+df = df.repartition(200)
+
+# split the data-frame
+train, test = df.randomSplit([0.8, 0.2], 42)
+
+# model: InceptionV3
+# extracting feature from images
+featurizer = DeepImageFeaturizer(inputCol="image",
+                                 outputCol="features",
+                                 modelName="InceptionV3")
+
+# used as a multi class classifier
+lr = LogisticRegression(maxIter=5, regParam=0.03, 
+                        elasticNetParam=0.5, labelCol="label")
+
+# define a pipeline model
+sparkdn = Pipeline(stages=[featurizer, lr])
+spark_model = sparkdn.fit(train) # start fitting or training
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+spark.stop()
