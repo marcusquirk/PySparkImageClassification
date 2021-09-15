1706b898009a3f4230cd0dadbbaaf2cf92846563
KatieWilson-Maher <62674873+KatieWilson-Maher@users.noreply.github.com>
2021-09-14 20:00:06 -0500

    Update mpiCombine.py

--- mpiCombine.py
+++ mpiCombine.py
@@ -1,8 +1,12 @@
+# Katie Wilson-Maher
+
 import os
 from mpi4py import MPI
 import math
 import shutil
 
+# Divides given list into a list of lists and returns the new list
+# Length of returned list is equal to the given number of processes
 def divideList(lst, processNum):
     start = 0
     end = 0
@@ -17,12 +21,12 @@ def divideList(lst, processNum):
 
     return lst1
 
-
+# Returns a list of bird species names that exist in both datasets
 def birdsInCommon(path1, path2):
     allFilesBirds = os.listdir(path1)
     allFilesCal = os.listdir(path2)
-    listAll = []
 
+    listAll = []
     for f1 in allFilesBirds:
         for f2 in allFilesCal:
             if f1 == f2:
@@ -30,7 +34,7 @@ def birdsInCommon(path1, path2):
 
     return listAll
 
-
+# Moves files from filePath2 to filePath1 if the paths have the same name
 def combineFiles(filePath1, filePath2):
     allFiles1 = os.listdir(filePath1)
     allFiles2 = os.listdir(filePath2)
@@ -52,6 +56,7 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 
 if rank == 0:
+    # Take input for paths to each folder
     path1 = input("First path: ")
     path2 = input("Second path: ")
 
@@ -59,35 +64,36 @@ else:
     path1 = None
     path2 = None
 
-
+# Send path1 and path2 to all processes
 path1 = comm.bcast(path1, root=0)
 path2 = comm.bcast(path2, root=0)
 
 if rank ==0:
 
-    # get list of matching files:
+    # Create a list of folder names where each folder name exists in both paths
     filePairs = birdsInCommon(path1, path2)
 
-    # divide list of matching files:
+    # Divide list of matching folder names into a new list with the size equal to the amount of processes
     pairsDivided = divideList(filePairs, size)
 
-    #deal with set 0:
+    # For each folder name in the set assigned to process 0, combine folders
     for files in pairsDivided[0]:
         combineFiles(path1, path2)
 
-    # send other sets of files to procs:
+    # Send other sets of folder names to each other process
     for proc in range(1, size):
         comm.send(pairsDivided[proc], dest=proc)
 
 
 else:
-
+    # Receive sets of folder names
     pairsDivided = comm.recv(source=0)
 
+    # For each folder name in the set for the current process, combine folders
     for setFiles in pairsDivided:
         for file in setFiles:
             combineFiles(path1, path2)
 
 
 # command:
-# mpiexec -np 2 python mpiCombine.py
+# mpiexec -np 4 python mpiCombine.py
