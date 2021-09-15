17889606d29446b9f9c2d7b345f19b75729ca431
Marcus <48571488+marcusquirk@users.noreply.github.com>
2021-09-15 01:28:29 -0500

    Add files via upload

--- resize_greyscale.py
+++ resize_greyscale.py
@@ -1,25 +1,30 @@
+#Marcus Quirk
+
 from PIL import Image
 import numpy as np
 import pathlib
 import os
 
-
+#Define constants for preprocessing
+#Hardcoded filepaths can be changed accordingly
 size = 200,200
 filepath = "./bird_images/test/"
 save_filepath = "./bird_images/test_resized/"
 directory = pathlib.Path(filepath)
 
-image_list = []
+#Create a list of all the categories (i.e. species of bird)
+categories = os.listdir(filepath)
 
-categories = list(os.walk(filepath))[0][1]
-
+#Iterate through each directory and create read and write filepaths
 for category in categories:
     cur_filepath = filepath + category
     cur_save = save_filepath + category
     os.mkdir(cur_save)
-    files = list(os.walk(cur_filepath))[0][2]
+    files = os.listdir(cur_filepath)
+    
+    #Iterate through the files in the read filepath
+    #Resize and convert each image to greyscale before saving to the write directory
     for file in files:
         image = Image.open(cur_filepath + '/' + file).convert('L')
         image = image.resize(size)
         image.save(cur_save + '/' + file)
-    print ('finished',str(category))
